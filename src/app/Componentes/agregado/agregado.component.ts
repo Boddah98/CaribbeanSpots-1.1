c6119be0775c221a5ab338b52ea4acb9a05a5676
Boddah98 <67991470+Boddah98@users.noreply.github.com>
2020-07-28 21:12:03 -0600

    Acomodo del boton y formulario en consola de navegador

--- src/app/Componentes/agregado/agregado.component.ts
+++ src/app/Componentes/agregado/agregado.component.ts
@@ -1,8 +1,28 @@
 import {Component} from '@angular/core'
+import { FormBuilder,FormGroup, Validators } from '@angular/forms';
 
 @Component({
     selector:'app-agregado',
     templateUrl: './agregado.component.html',
     styleUrls: ["../css/dimensionesCajas.css"]
+    
 })
-export class agregadoComponent{}
+export class agregadoComponent{
+    formulario_Contacto: FormGroup
+    constructor(
+        private _builder: FormBuilder
+    ){
+        this.formulario_Contacto = this._builder.group({
+            nombre:['',Validators.required],
+            apellido1:['',Validators.required],
+            apellido2:['',Validators.required],
+            celular1:['',Validators.required],
+            celular2:['',],
+            correo_Electronico:['',Validators.required]
+        })
+    }
+
+    agregar_Nuevo_Contacto(formulario_Contacto){
+        console.log(formulario_Contacto);
+    }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {inicioComponent} from './Componentes/inicio/inicio.component'
 import {agregadoComponent} from './Componentes/agregado/agregado.component'
+import { ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,inicioComponent,
@@ -15,7 +16,8 @@ import {agregadoComponent} from './Componentes/agregado/agregado.component'
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ReactiveFormsModule
   ],
   providers: [MatListModule],
   bootstrap: [AppComponent]
